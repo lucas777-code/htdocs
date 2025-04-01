c066e8c5668cfd1c5ced97c39c5531bfdf3e14ae
lucas777-code <lucascomparini0@gmail.com>
2025-04-01 14:19:46 -0300

    commit

--- Locadora_Veiculos/login.php
+++ Locadora_Veiculos/login.php
@@ -11,7 +11,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <style>
-
+            .login-container{
+                max-widht:400px;
+                margin: 100px auto;
+            }
+            .password-toggle{
+                position: absolute;
+                right: 15px;
+                top: 50%;
+                transform: translateY(-50%);
+                cursor: pointer;
+            }
         </style>
 </head>
 <body class="bg-light">
@@ -52,6 +62,14 @@
              </div>
         </div>
     </div>
+
+    <script>
+       function togglePassword() {
+            let passwordInput = document.getElementById('password');
+            passwordInput.type = (passwordInput.type === 'password') ?
+            'text' : 'password';
+        }
+    </script>    
     
 </body>
 </html>
